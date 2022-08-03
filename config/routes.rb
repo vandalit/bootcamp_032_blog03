b24621657e262d1ad67f84a27a9c39359a6206c5
Van <84937597+vandalit@users.noreply.github.com>
2022-08-03 19:30:26 -0400

    controlador_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
+  get 'pages/three'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
